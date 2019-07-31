4c2f0e99c8acb72e59c587213b2a1261d1e8c5c3
samzapata <legionario.zapata@gmail.com>
2019-07-31 00:37:38 -0500

    improve view of show site

--- app/helpers/sites_helper.rb
+++ app/helpers/sites_helper.rb
@@ -1,2 +1,16 @@
 module SitesHelper
+  NETWORKS = [
+    'link',
+    'facebook',
+    'instagram',
+    'twitter',
+    'youtube'
+  ]
+  def networks_section(site)
+    NETWORKS.each do |n|
+      if site[n] != nil || site[n] != ""
+        
+      end
+    end
+  end
 end

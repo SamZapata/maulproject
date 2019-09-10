8c1eda088ec5818e4c6e8dc4112dcfc08e50ebf0
samzapata <legionario.zapata@gmail.com>
2019-09-10 08:10:58 -0500

    improve seed information of communityies

--- db/communities.rb
+++ db/communities.rb
@@ -3,7 +3,7 @@
 communities = [
   {
     name: "Comunidad Santiaguina",
-    about: "Somos estudiantes, egresados, profesores y personas allegadas a la Universidad Santiago de Cali",
+    about: "Somos estudiantes, egresados, profesores, trabajadores y personas que nos sentimos parte de la Universidad Santiago de Cali. Somos santiaguinos, somos USC.",
     email: "",
     website: "http://usc.edu.co",
     phone: "",
@@ -14,8 +14,8 @@ communities = [
     image: 'images/com-1'
   },
   {
-    name: "Somos Lobos - Rugby",
-    about: "Somos la comunidad de Lobos Rugby Club",
+    name: "Somos Lobos Rugby",
+    about: "Somos lobos y lobas, la comunidad de lobos rugby club y lobos univalle. Somos jugadores, entrenadores, exjugadores, hinchas y quienes admiramos y apoyamos el progreso del equipo desde todas sus categorías y representaciones...Y sepan que solo muero si ustedes van aflojando porque el que murió peleando vive en cada compañer@. Venceremos!",
     email: "",
     website: "",
     phone: "317 5571203",
@@ -27,7 +27,7 @@ communities = [
   },
   {
     name: "Niños, Niñas, Adolescentes y Jóvenes Gestores de Paz - Cali",
-    about: "Movimiento Nacional de niños, niñas, adolescentes y jóvenes gestores de paz de Santiago de Cali",
+    about: "Movimiento Nacional de niños, niñas, adolescentes y jóvenes gestores de paz de Santiago de Cali. Somos personas que le aportamos a la construcción de una cultura de paz desde nuestros entornos, haciendo actividades que permitan vivenciar experiencias de transformación social y que reivindican los derechos y buenas prácticas niños, niñas y jóvenes en lugares con altos índices de problemáticas sociales.",
     email: "",
     website: "https://www.wvi.org/es/GestoresdePaz",
     phone: "",
@@ -39,7 +39,7 @@ communities = [
   },
   {
     name: "Distrito Rugby",
-    about: "Comunidad de personas simpatizantes por el desarrollo y difusión del rugby",
+    about: "Comunidad de personas simpatizantes por el desarrollo y difusión del rugby, que empieza promocionando este apasionante deporte en el Distrito de Aguablanca de Santiago de Cali. Creemos que el rugby es un estilo de vida y herramienta que aporta y forja importantes valores en el desarrollo de las personas que practican esta disciplina deportiva.",
     email: "",
     website: "http://distritorugby.herokuapp.com/",
     phone: "",
@@ -51,7 +51,7 @@ communities = [
   },
   {
     name: "Rugby Valle",
-    about: "Buscamos promover el desarrollo del rugby en el Valle de Cauca",
+    about: "Buscamos promover el desarrollo del rugby en el Valle de Cauca. Somos la comunidad que práctica y promueve el rugby en todo el departamento.",
     email: "",
     website: "http://rugbyvalle.co/",
     phone: "",
@@ -61,6 +61,66 @@ communities = [
     comuna_id: 19,
     image: 'images/com-5'
   },
+  {
+    name: "Soy Sultán",
+    about: "Apoyamos a Sultanes Rugby Club",
+    email: "sultanes.rc@hotmail.com",
+    website: "http://www.sultanesrugby.club/",
+    phone: "311 3007050",
+    facebook: "https://www.facebook.com/SultanesRugby/",
+    instagram: "https://www.instagram.com/sultanes_rc/",
+    twitter: ""
+  },
+  {
+    name: "Buziracas Rugby",
+    about: "Apoyamos el desarrollo del rugby femenino, somos buziracas hu-hu-hu!",
+    email: "buziracasrugbyfem@gmail.com",
+    website: "",
+    phone: "",
+    facebook: "https://www.facebook.com/buziracasrugbyclub/",
+    instagram: "https://www.instagram.com/buziracas_rugby/",
+    twitter: ""
+  },
+  {
+    name: "Phalanx rugby",
+    about: "Somos la comunidad de Phalanx Rugby Club. Sentimiento azul y negro. Las heridas sanan, los huesos soldan, la moral se levanta.",
+    email: "",
+    website: "",
+    phone: "316 4212096",
+    facebook: "https://www.facebook.com/Phalanxrc/",
+    instagram: "https://www.instagram.com/phalanxrugbyclub/",
+    twitter: ""
+  },
+  {
+    name: "Tikunas Rugby",
+    about: "Somos la comunidad de personas que apoya al club de rugby Tikunas. Equipo de rugby femenino que impulsa el desarrollo de este deporte en el sur y oriente de la ciudad. Hasta el alma",
+    email: "",
+    website: "",
+    phone: "318 4031176",
+    facebook: "https://www.facebook.com/TikunasRugbyClubCali/",
+    instagram: "https://www.instagram.com/tikunasrugby/",
+    twitter: ""
+  },
+  {
+    name: "Amapolas rugby",
+    about: "Somos la comunidad que apoya a Amapolas Rugby Club. Equipo de rugby femenino que entrena en el norte de la ciudad.",
+    email: "",
+    website: "",
+    phone: "3173227172",
+    facebook: "https://www.facebook.com/Amapolas-Buziracos-Rugby-Club-266240000248634/?__xts__[0]=68.ARBIAv6e1KFrX8KjDFq_vcWJkF_izQp_8cNwSgct2p4-_VDeea4gMVbbqDwuTJOi-AMAvFM9KjwUDjLED--P_MpdnNOTNLYWqEf1rPMYg5j8gF2v7WsRlmnXAUzgeU7pN4yyX9Y20SCzXzQLl-FrH0Q9gh3wMZP6lmZA-kl4Kj49QjBtBga5UoFn4FBznpb9tpeAwoZDzXgMg3KgLXKJj8CwcISVlb3Fmulp1bSFTT4uYyJjAXq7LgCKjNKd_OwULM1GmlJn-oTe",
+    instagram: "https://www.instagram.com/amapolasrc/?hl=bg",
+    twitter: ""
+  },
+  # {
+  #   name: "",
+  #   about: "",
+  #   email: "",
+  #   website: "",
+  #   phone: "",
+  #   facebook: "",
+  #   instagram: "",
+  #   twitter: ""
+  # },
   # {
   #   name: "",
   #   about: "",
